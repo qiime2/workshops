7f572c598ca56b715330072171bee436f215b20a
Jorden <jakereps@users.noreply.github.com>
2016-08-04 14:00:25 -0700

    ENH: django admin modifications
    Pair-programmed with @thermokarst

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class InstructorInline(admin.StackedInline):
+    model = Instructor.workshops.through
+    extra = 3
+    verbose_name = 'Instructor'
+    verbose_name_plural = 'Instructors'
+
+
+class WorkshopAdmin(admin.ModelAdmin):
+    inlines = [InstructorInline]
+    prepopulated_fields = {'slug': ('title', 'start_date')}
+    list_display = ('title', 'start_date', 'end_date', 'url')
+
+
+class RateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'workshop')
+
+
+class OrderItemInline(admin.TabularInline):
+    can_delete = False
+    model = OrderItem
+    extra = 0
+    readonly_fields = ('order', 'rate', 'quantity')
+
+    def has_add_permission(self, request):
+        return False
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+    readonly_fields = ('email', 'order_total', 'billed_total',
+                       'billed_datetime')
+    list_display = ('email', 'order_total', 'order_datetime',
+                    'billed_total', 'billed_datetime')
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'rate', 'quantity')
+    readonly_fields = ('order', 'rate', 'quantity')
+
+admin.site.register(Workshop, WorkshopAdmin)
+admin.site.register(Instructor)
+admin.site.register(Rate, RateAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
--- payments/models.py
+++ payments/models.py
@@ -6,8 +6,9 @@ class Workshop(models.Model):
     description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
-    url = models.URLField(max_length=2000)
-    slug = models.SlugField()
+    url = models.URLField(verbose_name='URL', max_length=2000)
+    slug = models.SlugField(help_text='This is the unique identifier for the '
+                            'URL (i.e. title-YYYY-MM-DD)')
 
     def __str__(self):
         return self.title
@@ -24,19 +25,30 @@ class Instructor(models.Model):
 class Rate(models.Model):
     workshop = models.ForeignKey(Workshop)
     name = models.CharField(max_length=300)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2,
+                                verbose_name='price (USD)')
 
     def __str__(self):
-        return '%s: %s' % (self.name, self.price)
+        return '%s: $%s' % (self.name, self.price)
 
 
 class Order(models.Model):
     email = models.EmailField()
-    order_total = models.DecimalField(max_digits=7, decimal_places=2)
+    order_total = models.DecimalField(max_digits=7, decimal_places=2,
+                                      verbose_name='order total (USD)')
     order_datetime = models.DateTimeField(auto_now_add=True)
-    billed_total = models.DecimalField(max_digits=7, decimal_places=2,
-                                       null=True)
-    billed_datetime = models.DateTimeField(null=True)
+    billed_total = models.DecimalField(
+        max_digits=7,
+        decimal_places=2,
+        null=True,
+        verbose_name='billed total (USD)',
+        help_text='This is the confirmed paid amount from NAU'
+    )
+    billed_datetime = models.DateTimeField(
+        null=True,
+        help_text='This is the confirmed date and time of payment',
+        verbose_name='billed date & time'
+    )
 
     def __str__(self):
         return '%s: $%s on %s' % (self.email, self.order_total,
