8d77a8bf03438f95b3ff508aa0a8bc86d4f48f87
Matthew Dillon <matthewrdillon@gmail.com>
2016-08-10 09:18:31 -0700

    ENH: Add Django Debug Toolbar for development

--- payments/middleware.py
+++ payments/middleware.py
@@ -1,8 +1,13 @@
 from django.utils.deprecation import MiddlewareMixin
 
 from subdomains.middleware import SubdomainURLRoutingMiddleware
+from debug_toolbar.middleware import DebugToolbarMiddleware
 
 
 class PatchedSubdomainURLRoutingMiddleware(MiddlewareMixin,
                                            SubdomainURLRoutingMiddleware):
+    pass
+
+
+class PatchedDebugToolbarMiddleware(MiddlewareMixin, DebugToolbarMiddleware):
     pass
