3a30e42fc8dc088a8421910b7c9c155e6bb8e282
Jorden Kreps <jakereps@users.noreply.github.com>
2016-08-11 10:31:20 -0700

    DOC: Add license headers (#36)

--- config/urls/payments.py
+++ config/urls/payments.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016--, QIIME development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
