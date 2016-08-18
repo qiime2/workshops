d6a1741c68557926327d3cbec5f832e873dd1de7
Matthew Dillon <matthewrdillon@gmail.com>
2016-08-17 20:43:19 -0700

    WIP: POC, need to DRY

--- payments/admin.py
+++ payments/admin.py
@@ -56,20 +56,92 @@ class OrderItemInline(admin.TabularInline):
         return False
 
 
+class OrderPaidListFilter(admin.SimpleListFilter):
+    title = 'payment status'
+    parameter_name = 'paid'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('true', 'Paid'),
+            ('false', 'Not Paid'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'true':
+            return queryset.exclude(billed_total='')
+        if self.value() == 'false':
+            return queryset.filter(billed_total='')
+
+
+class OrderWorkshopFilter(admin.SimpleListFilter):
+    title = 'workshop'
+    parameter_name = 'workshop'
+
+    def lookups(self, request, model_admin):
+        return Workshop.objects.values_list('pk', 'title') \
+                .order_by('start_date')
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(orderitem__rate__workshop=self.value())
+
+
 class OrderAdmin(admin.ModelAdmin):
     inlines = [OrderItemInline]
-    readonly_fields = ('contact_email', 'order_total', 'billed_total',
-                       'billed_datetime', 'transaction_id')
+    readonly_fields = ('contact_name', 'contact_email', 'order_total',
+                       'billed_total', 'billed_datetime', 'transaction_id')
     list_display = ('contact_name', 'contact_email', 'order_total',
                     'order_datetime', 'billed_total', 'billed_datetime',
                     'transaction_id')
-    list_filter = ('order_datetime', 'contact_email', 'billed_datetime',
-                   'billed_total')
+    list_display_links = ('contact_name', 'contact_email')
+    list_filter = (OrderPaidListFilter, OrderWorkshopFilter, 'order_datetime',
+                   'contact_email')
+
+
+class OrderItemWorkshopFilter(admin.SimpleListFilter):
+    title = 'workshop'
+    parameter_name = 'workshop'
+
+    def lookups(self, request, model_admin):
+        return Workshop.objects.values_list('pk', 'title') \
+                .order_by('start_date')
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(rate__workshop=self.value())
+
+
+class OrderItemPaidListFilter(admin.SimpleListFilter):
+    title = 'payment status'
+    parameter_name = 'paid'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('true', 'Paid'),
+            ('false', 'Not Paid'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'true':
+            return queryset.exclude(order__billed_total='')
+        if self.value() == 'false':
+            return queryset.filter(order__billed_total='')
 
 
 class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ('order', 'rate', 'email')
-    readonly_fields = ('order', 'rate', 'email')
+    list_display = ('name', 'email', 'workshop', 'rate',
+                    'order_transaction_id')
+    list_filter = (OrderItemWorkshopFilter, OrderItemPaidListFilter)
+    readonly_fields = ('order', 'rate', 'name', 'email')
+
+    def order_transaction_id(self, obj):
+        return obj.order.transaction_id
+    order_transaction_id.admin_order_field = 'order__transaction_id'
+
+    def workshop(self, obj):
+        return obj.rate.workshop.title
+    workshop.admin_order_field = 'rate__workshop__title'
+
 
 admin.site.register(Workshop, WorkshopAdmin)
 admin.site.register(Instructor)
