bb9a6f1143208353a85ed76bf4d4e3d3a818fb8b
Matthew Dillon <matthewrdillon@gmail.com>
2016-12-23 14:51:14 -0700

    ENH: Update LICENSE; update header text; q2lint

--- manage.py
+++ manage.py
@@ -1,7 +1,5 @@
-#!/usr/bin/env python
-
 # ----------------------------------------------------------------------------
-# Copyright (c) 2016--, QIIME development team.
+# Copyright (c) 2016-2017, QIIME 2 development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
