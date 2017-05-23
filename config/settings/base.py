fc428a7898fc3186fd6cb6c5696669be2a41cad9
Matthew Dillon <matthewrdillon@gmail.com>
2017-05-23 11:46:43 -0700

    BUG: Disabling extended cookie validation for now

--- config/settings/base.py
+++ config/settings/base.py
@@ -66,7 +66,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'payments.middleware.CookieValidationMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls.payments'
