778885dfff87fce3bef8e9d11dde7c86b1adee0b
Matthew Dillon <matthewrdillon@gmail.com>
2019-03-01 13:14:43 -0700

    BUG: postgres in travis (#115)

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,9 +2,6 @@ from .shared import (
     env,
     BASE_DIR,
     PROJ_DIR,
-    SECRET_KEY,
-    DEBUG,
-    ALLOWED_HOSTS,
     INSTALLED_APPS,
     MIDDLEWARE,
     ROOT_URLCONF,
@@ -21,18 +18,6 @@ from .shared import (
     STATICFILES_DIRS,
     STATIC_ROOT,
     APPEND_SLASH,
-    EMAIL_BACKEND,
-
-    # BUSINESS LOGIC
-    ADMINS,
-    LMID,
-    PAYMENT_URL,
-    PAYMENT_TITLE,
-    PAYMENT_DESCRIPTION,
-    PAYMENT_CONTACT_INFO,
-    PSF_SPEEDTYPE,
-    PSF_ACCT_NUMBER,
-    TECHNICAL_CONTACT,
 )
 
 
