157d801a261630c801ddc4b4183840195064c77c
Matthew Dillon <matthewrdillon@gmail.com>
2020-09-16 11:14:02 -0700

    squash

--- workshops/payments/management/commands/release_tickets.py
+++ workshops/payments/management/commands/release_tickets.py
@@ -1,23 +1,20 @@
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
-from workshops.payments.models import Workshop, Rate
+from workshops.payments.models import Rate
 
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
-        parser.add_argument('workshop_id', type=int)
         parser.add_argument('rate_id', type=int)
         parser.add_argument('tickets_to_add', type=int)
         parser.add_argument('max_ticket_capacity', type=int)
 
     def handle(self, *args, **options):
-        workshop_id = options['workshop_id']
         rate_id = options['rate_id']
         tickets_to_add = options['tickets_to_add']
         max_ticket_capacity = options['max_ticket_capacity']
 
-        workshop = Workshop.objects.get(id=workshop_id)
         rate = Rate.objects.get(id=rate_id)
         private_rates = Rate.objects.filter(private=True, parent=rate,
                                             sold_out=False, sales_open=False)
