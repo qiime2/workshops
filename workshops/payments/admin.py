008da4a3f51f3443efb8901f760b19740c0d9a90
Keegan Evans <keegan.evans@gmail.com>
2021-03-08 20:56:25 -0700

    doc: added headers and updated copyright

--- workshops/payments/admin.py
+++ workshops/payments/admin.py
@@ -1,3 +1,10 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2021, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
 from django.contrib import admin
 from django.urls import resolve
 from django.utils.html import format_html_join, format_html
